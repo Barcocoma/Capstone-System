4f178606b3cdcb673747ec2ce83d843b91455c60
Your Name <barcomajoshua87@gmail.com>
2025-06-16 23:26:29 +0800

    Create dashboard.jsx

---
+++ Capstone 2/src/layouts/dashboard.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Sidenav, DashboardNavbar, Configurator } from "@/widgets/layout";
+import routes from "@/routes";
+import { useMaterialTailwindController, setOpenConfigurator } from "@/context";
+import { IconButton } from "@material-tailwind/react";
+import { Cog6ToothIcon } from "@heroicons/react/24/outline";
+
+export function Dashboard() {
+  const [controller, dispatch] = useMaterialTailwindController();
+  const { sidenavType } = controller;
+
+  return (
+    <div className="min-h-screen bg-blue-gray-50/50">
+      <Sidenav
+        routes={routes}
+        brandImg={
+          sidenavType === "dark" ? "/img/logo-ct.png" : "/img/logo-ct-dark.png"
+        }
+      />
+      <div className="p-4 xl:ml-80">
+        <DashboardNavbar />
+        <Configurator />
+        <IconButton
+          size="lg"
+          color="white"
+          className="fixed bottom-8 right-8 z-40 rounded-full shadow-blue-gray-900/10"
+          ripple={false}
+          onClick={() => setOpenConfigurator(dispatch, true)}
+        >
+          <Cog6ToothIcon className="h-5 w-5" />
+        </IconButton>
+        <Routes>
+          {routes.map(
+            ({ layout, pages }) =>
+              layout === "dashboard" &&
+              pages.map(({ path, element }) => (
+                <Route exact path={path} element={element} />
+              ))
+          )}
+          <Route path="*" element={<Navigate to="/dashboard/home" replace />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
