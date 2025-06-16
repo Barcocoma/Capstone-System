0123994d8b4c7d4614e89f82a8fa074c8cd7e2b0
Your Name <barcomajoshua87@gmail.com>
2025-06-16 23:25:51 +0800

    Create customer-table-data.js

---
+++ Capstone 2/src/data/customer-table-data.js
@@ -0,0 +1,22 @@
+export const customerTableData = [
+  {
+    id: 1,
+    fullName: "A-03",
+    section: "A",
+    sector: "Alpha",
+    ownerName: "Maria Santos",
+    status: "Sold",
+    lastPayment: "May 2025",
+  },
+  {
+    id: 2,
+    fullName: "B-07",
+    section: "B",
+    sector: "Beta",
+    ownerName: "Jim Kupal",
+    status: "Delinquent",
+    lastPayment: "March 2025",
+  },
+];
+
+export default customerTableData; 
